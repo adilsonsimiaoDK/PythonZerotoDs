81eaf2950572b256f53d3da96453f192937c53e3
adilsonsimiaoDK <adilsonsimiaock@gmail.com>
2021-04-02 15:36:15 +0200

    Examples 1.1 finished

--- example1.1.py
+++ example1.1.py
@@ -44,6 +44,19 @@ data = data.drop(cols_del, axis=1)
 # print(data.loc[data['condition_type'] == 'regular'].shape)
 # print(data[((data['house_age'] == 'new_house') & (data['condition_type'] == 'good'))])
 # print(data[((data['condition_type'] == 'bad') & (data['waterfront'] == 1))])
+# print(data[['condition_type', 'price']].loc[data['dormitory_type'] == 'studio'].sort_values('price', ascending=False))
+# print(data[((data['dormitory_type'] == 'apartment') & (data['yr_renovated'] == 2015).shape)])
+# print(data[['dormitory_type','bedrooms']].loc[data['dormitory_type']=='house'].sort_values('bedrooms',ascending=False))
+# print(data[(data['house_age']=='new_house') & (data['yr_renovated']=='2014')].shape)
+# print(data[['id', 'date', 'price', 'floors', 'zipcode']])
+# print(data.iloc[:,[0,1,2, 7, 16]])
+# col = data[['id', 'date', 'price', 'floors', 'zipcode']]
+# print(data.loc[0:5, col])
+# print(data.shape)
+# cols=[True,True,True,False,False,False,False,
+#         True,False,False,False,False,False,False,False,
+#         False,True,False,False,False,False,False]
+# print(data.loc[:, cols])
 
 # ========================================================
 # select data by index and columns name with loq quantity rows 0:n qat col 0:name
@@ -55,29 +68,20 @@ data = data.drop(cols_del, axis=1)
 #         'False', 'False', 'False', 'False', 'False', 'False', 'False', 'False']
 # print(data.loc[0:10, cols])
 
-# ============================================
-# How many properties and o max number of the rooms
-# print(data[data['floors'] == 3.5][['id', 'floors']])
-# print(data[data['floors'] == 3.5].shape)
-# ==============================================
-# # created columns level, select high and small price after that classification the home
-# data['level'] = 'standard'
-# data.loc[data['price'] > 540000, 'level'] = 'high_standard'
-# data.loc[data['price'] < 540000, 'level'] = 'low_standard'
-# print(data.sort_values('price', ascending=False))
+
 # ===============================================
 # A report of houses sorted by prices
-# report = data[['id', 'date', 'price', 'bedrooms', 'sqft_lot', 'level']].sort_values('price', ascending=False)
-# report.to_csv('data/report01.csv', index=False)
+report = data[['id', 'date', 'price', 'floors', 'zipcode']].sort_values('price', ascending=False)
+report.to_csv('data/report1.1.csv', index=False)
 # print(report.head())
 # ==============================================
 # libraries that drawing maps Plotly
-# data_maps = data[['id','lat', 'long', 'price']]
-# mapss = px.scatter_mapbox(data_maps, lat='lat', lon='long',
-#                           hover_name='id',
-#                           hover_data=['price'],
-#                           color_discrete_sequence=['orange'], zoom=6, height=300)
-# mapss.update_layout(mapbox_style='open-street-map')
-# mapss.update_layout(height=1080, margin={'r':0, 't':0, 'l':0, 'b':0})
-# mapss.show()
+data_maps = data[['id','lat', 'long', 'price']]
+mapss = px.scatter_mapbox(data_maps, lat='lat', lon='long',
+                          hover_name='id',
+                          hover_data=['price'],
+                          color_discrete_sequence=['pink'], zoom=6, height=300)
+mapss.update_layout(mapbox_style='open-street-map')
+mapss.update_layout(height=1080, margin={'r':0, 't':0, 'l':0, 'b':0})
+mapss.show()
 # mapss.write_html('data/map_house.html')
