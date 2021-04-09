13391e9d4ed7d800fac51256e3ccfd89247f81e5
adilsonsimiaoDK <adilsonsimiaock@gmail.com>
2021-04-09 19:47:44 +0200

    Examples03

--- example03.py
+++ example03.py
@@ -2,6 +2,18 @@ import pandas as pd
 import requests as r
 
 data = pd.read_csv('data/kc_house_data.csv')
-url = 'https://www.linkedin.com/jobs/'
-response = r.request('GET',url)
-print(response.json())
+
+i = 1
+while True:
+    print('page:{}'.format(i))
+    url = 'https://www.linkedin.com/jobs/'
+    response = r.request('GET',url)
+
+    if response != []:
+        data = response.text
+        df = pd.DataFrame(data, index=[0])
+        print(df[['id', 'type']])
+        dataset = pd.concat([dataset, df], axis=0)
+        i += 1
+    else:
+        break
